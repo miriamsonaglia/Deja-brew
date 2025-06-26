528bf8149353ae331267c6f11440baf16fbe89cb
Lorenzo Dall'Ara <lorenzo.dallara2@studio.unibo.it>
2025-06-26 16:29:36 +0200

    fixed models

--- app/Models/eloquent_models.php
+++ app/Models/eloquent_models.php
@@ -3,14 +3,14 @@
 // Model: User (Utente)
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'nome',
@@ -27,7 +27,7 @@ class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        // Rimuoviamo 'password' => 'hashed' se non vuoi hashing automatico
     ];
 
     // Relazioni
