81476938636f7c00ebd744f6b269d4999c4b1976
Alessandro_Buono <alessandro.buono4@studio.unibo.it>
2025-06-26 17:16:47 +0200

    Created impostazioniUtente DB table

--- database/migrations/2025_06_26_150349_create_impostazioni_utente_table.php
+++ database/migrations/2025_06_26_150349_create_impostazioni_utente_table.php
@@ -4,24 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateImpostazioniUtenteTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Esegui la migrazione.
      */
     public function up(): void
     {
-        Schema::create('impostazioni_utente', function (Blueprint $table) {
+        Schema::create('impostazioniUtente', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_utente');
+            $table->string('tema')->nullable();
+            $table->boolean('notifiche')->default(true);
             $table->timestamps();
+
+            $table->foreign('id_utente')->references('id')->on('utente')->onDelete('cascade');
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Annulla la migrazione.
      */
     public function down(): void
     {
-        Schema::dropIfExists('impostazioni_utente');
+        Schema::dropIfExists('impostazioniUtente');
     }
-};
+}
