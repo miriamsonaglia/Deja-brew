7aad4d109c6c1e93050c2937df7690c8f687de88
Alessandro_Buono <alessandro.buono4@studio.unibo.it>
2026-01-08 23:06:26 +0100

    Fixed datetime to beeing accessible to firefox users

--- checkout.php
+++ checkout.php
@@ -318,6 +318,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'] ?? '') === 'add_c
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+<script>
+    const scadenzaInput = document.getElementById('scadenza');
+		if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+			scadenzaInput.setAttribute('placeholder', 'YYYY-MM');
+		} else {
+			scadenzaInput.setAttribute('type', 'month');
+		}
+</script>
 </body>
 </html>
