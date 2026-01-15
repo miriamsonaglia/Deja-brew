4672be72fcab9e16b94ba09a6b3f976c0e14ff3b
Alessandro_Buono <alessandro.buono4@studio.unibo.it>
2026-01-15 16:39:13 +0100

    Not logged user navbar fixed home link

--- reusables/navbars/empty-navbar.php
+++ reusables/navbars/empty-navbar.php
@@ -1,6 +1,7 @@
 <nav class="navbar bg-light border-bottom">
   <div class="container-fluid justify-content-between align-items-center">
-    <span class="navbar-brand mx-auto fw-bold">Deja-brew</span>
+    <span></span>
+    <a href='./home.php' class="nav-link-custom"><span class="navbar-brand mx-auto fw-bold fs-3">Deja-brew</span></a>
     <div class="d-flex align-items-center">
       <a href="./login.php">Accedi</a>
     </div>
