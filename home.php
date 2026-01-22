686a5a39a1067ecb83cbe5ecb5222b6639434a1b
Miriam Sonaglia <sonagliamiriam@gmail.com>
2026-01-22 18:48:35 +0100

    .

--- home.php
+++ home.php
@@ -123,7 +123,7 @@
             ?>
         </div>
 
-        <footer><!-- ?? Possible footer template ?? --></footer>
+        <?php require_once __DIR__ . '/reusables/footer.php' ?>
 
         <!-- INSERT HERE ALL JAVASCRIPT NECESSARY IMPORTS -->
         <script src="./dist/bootstrap5/js/bootstrap.min.js"></script>
